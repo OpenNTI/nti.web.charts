ec2b534d50559db04b3f232f09ad2fcbae7bc7f3
Cory Jones <cory.jones@nextthought.com>
2018-02-20 12:38:11 -0600

    Tweak canvas size and make appearance more customizable

--- src/circular-progress/CircularProgress.jsx
+++ src/circular-progress/CircularProgress.jsx
@@ -52,6 +52,20 @@ export default class CircularProgress extends React.Component {
 		pixelDensity: PropTypes.number,
 
 
+		/**
+		 * Factor of how thick the progress line should be
+		 *
+		 * @type {number} Default: 1
+		 */
+		lineThickness: PropTypes.number,
+
+		/**
+		 * If specified, will fill the rest of the circle with this color
+		 *
+		 * @type {string} Default: undefined
+		 */
+		deficitFillColor: PropTypes.string,
+
 		withLetter: PropTypes.bool,
 
 		/**
@@ -75,6 +89,7 @@ export default class CircularProgress extends React.Component {
 		pixelDensity: (global.devicePixelRatio || 1) * 2,
 		width: 200,
 		height: 200,
+		lineThickness: 1,
 		showValue: true,
 		showPctSymbol: true
 	}
@@ -135,10 +150,10 @@ export default class CircularProgress extends React.Component {
 
 	drawCircle = (ctx) => {
 		let node = ctx.canvas,
-			stroke = node.width * (1 / 112),
+			stroke = node.width * (1 / (112 / (this.props.lineThickness || 1))),
 			centerX = node.width / 2,
 			centerY = node.height / 2,
-			radius = node.width / 4,
+			radius = (node.width / 2) - (2 * this.props.lineThickness || 1),
 			textbox,
 			value = (this.props.value || 0).toString(10),
 			font = {
@@ -157,20 +172,26 @@ export default class CircularProgress extends React.Component {
 			ctx.fillStyle = this.getColor();
 			ctx.strokeStyle = this.props.strokeColor || this.getColor();
 
-			//ctx.shadowColor = '#ddd';
-			//ctx.shadowBlur = 5;
-			//ctx.shadowOffsetX = 1;
-			//ctx.shadowOffsetY = 1;
-
 			ctx.stroke();
+
+			if(this.props.deficitFillColor) {
+				ctx.beginPath();
+				ctx.arc(0, 0, radius, (2 * Math.PI) * (value / 100), 0, false);
+				ctx.lineWidth = stroke;
+				ctx.fillStyle = this.getColor();
+				ctx.strokeStyle = this.props.deficitFillColor;
+
+				ctx.stroke();
+			}
 
 			ctx.lineWidth = 0;
 			ctx.setTransform(1, 0, 0, 1, centerX, centerY);
 			setFont(ctx, font);
 			textbox = ctx.measureText(value);
-			ctx.globalAlpha = 0.8;
 
 			if(this.props.showValue) {
+				font.weight = '600';
+				setFont(ctx, font);
 				ctx.fillText(value, -textbox.width / 2, font.size / 3);
 
 				font.size /= 3;
