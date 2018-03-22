2d8c639d2c93cdc9f452cda8b0d1fc1032b2154d
Cory Jones <cory.jones@nextthought.com>
2018-03-22 11:33:11 -0500

    Use svg progress circles

--- src/circular-progress/CircularProgress.jsx
+++ src/circular-progress/CircularProgress.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 export default class CircularProgress extends React.Component {
 
 	static propTypes = {
-		/**
-		 * Specifying a singular color will result in drawing only one segment.
-		 * Adding a second color will make this draw the second segment.
-		 *
-		 * @type {String/Array[String]} The color to fill the segment(s) with
-		 */
-		color: PropTypes.oneOfType([
-			//Draw only one segement
-			PropTypes.string,
-			//Draw both segments
-			PropTypes.arrayOf(PropTypes.string)
-		]),
-
 		/**
 		 * Optional stroke color.
 		 *
@@ -45,243 +33,89 @@ export default class CircularProgress extends React.Component {
 		height: PropTypes.number,
 
 		/**
-		 * The pixel density to render at.
-		 *
-		 * @type {number} Default: 2
-		 */
-		pixelDensity: PropTypes.number,
-
-
-		/**
-		 * Factor of how thick the progress line should be
-		 *
-		 * @type {number} Default: 1
-		 */
-		lineThickness: PropTypes.number,
-
-		/**
 		 * If specified, will fill the rest of the circle with this color
 		 *
 		 * @type {string} Default: undefined
 		 */
 		deficitFillColor: PropTypes.string,
 
-		withLetter: PropTypes.bool,
-
 		/**
-		 * Whether to show numeric value
+		 * Is in completion state (show checkmark rather than 100%)
 		 *
-		 * @type {bool} Default: true
+		 * @type {bool} Default: undefined
 		 */
-		showValue: PropTypes.bool,
-
-		/**
-		 * Whether to show % symbol
-		 *
-		 * @type {bool} Default: true
-		 */
-		showPctSymbol: PropTypes.bool
+		isComplete: PropTypes.bool
 	}
 
 	static defaultProps = {
 		value: 90,
-		color: '#40b450',//#a5c959
-		pixelDensity: (global.devicePixelRatio || 1) * 2,
+		strokeColor: '#3fb34f',
+		deficitFillColor: '#eee',
 		width: 200,
-		height: 200,
-		lineThickness: 1,
-		showValue: true,
-		showPctSymbol: true
-	}
-
-	attachRef = x => this.canvas = x
-
-	componentDidMount () {
-		const {canvas} = this;
-		let context = canvas.getContext('2d');
-
-		context.imageSmoothingEnabled = true;
-
-		this.paint(context);
-	}
-
-	componentDidUpdate () {
-		this.paint(this.canvas.getContext('2d'));
+		height: 200
 	}
 
 	render () {
-		let p = this.props;
-		let width = p.width * p.pixelDensity;
-		let height = p.height * p.pixelDensity;
-		let style = {
-			width: p.width,
-			height: p.height
+		const {value, strokeColor, deficitFillColor, isComplete} = this.props;
+
+		if(isComplete) {
+			const fontSize = this.props.width / 1.5;
+
+			let completedStyle = {
+				width: this.props.width,
+				height: this.props.height,
+				borderRadius: this.props.width
+			};
+
+			let checkStyle = {
+				fontSize,
+				width: this.props.width,
+				top: this.props.height / 10
+			};
+
+			return (
+				<div className="circular-progress progress-complete-container">
+					<div style={completedStyle} className="progress-completed"><div style={checkStyle} className="check"><i className="icon-check" /></div></div>
+				</div>
+			);
+		}
+
+		const fontSize = this.props.width / 3;
+		const top = this.props.height / 4;
+
+		let valueStyle = {
+			fontSize,
+			width: this.props.width,
+			top
 		};
 
-		const otherProps = {...this.props};
-
-		delete otherProps.deficitFillColor;
-		delete otherProps.showValue;
-		delete otherProps.showPctSymbol;
-		delete otherProps.pixelDensity;
-		delete otherProps.lineThickness;
+		const className = cx('circular-progress', {'no-progress': value === 0});
 
 		return (
-			<canvas ref={this.attachRef} {...otherProps} className="value" style={style} width={width} height={height} />
+			<div className={className}>
+				<svg width={this.props.width} height={this.props.height} viewBox="0 0 36 36">
+					<path
+						d="M18 2.0845
+					a 15.9155 15.9155 0 0 1 0 31.831
+					a 15.9155 15.9155 0 0 1 0 -31.831"
+						fill="none"
+						stroke={deficitFillColor}
+						strokeWidth="2"
+						strokeDasharray="100, 100"
+					/><path
+						d="M18 2.0845
+					a 15.9155 15.9155 0 0 1 0 31.831
+					a 15.9155 15.9155 0 0 1 0 -31.831"
+						fill="none"
+						stroke={strokeColor}
+						strokeWidth="2"
+						strokeDasharray={`${value}, 100`}
+					/>
+				</svg>
+				<div className="value" style={valueStyle}>
+					{value}
+				</div>
+			</div>
 		);
 	}
-
-	paint = (context) => {
-		context.canvas.width += 0; //set the canvas dirty and make it clear on next draw.
-
-		this.drawCircle(context);
-		if (this.props.withLetter) {
-			this.drawDot(context);
-		}
-	}
-
-	getColor = () => {
-		let c = this.props.color;
-		if (Array.isArray(c)) {
-			c = c[0];
-		}
-		return c;
-	}
-
-	getSecondaryColor = () => {
-		let c = this.props.color;
-		if (!Array.isArray(c)) {
-			c = [];
-		}
-		return c[1];
-	}
-
-	drawCircle = (ctx) => {
-		let node = ctx.canvas,
-			stroke = node.width * (1 / (112 / (this.props.lineThickness || 1))),
-			centerX = node.width / 2,
-			centerY = node.height / 2,
-			radius = (node.width / 2) - (2 * this.props.lineThickness || 1),
-			textbox,
-			value = (this.props.value || 0).toString(10),
-			font = {
-				size: Math.floor(radius * 0.8),
-				weight: '300'
-			};
-
-		ctx.save();
-		try {
-			ctx.setTransform(1, 0, 0, 1, 0, 0);
-			ctx.translate(centerX, centerY);
-			ctx.rotate(-Math.PI / 2);
-			ctx.beginPath();
-			ctx.arc(0, 0, radius, 0, (2 * Math.PI) * (value / 100), false);
-			ctx.lineWidth = stroke;
-			ctx.fillStyle = this.getColor();
-			ctx.strokeStyle = this.props.strokeColor || this.getColor();
-
-			ctx.stroke();
-
-			if(this.props.deficitFillColor) {
-				ctx.beginPath();
-				ctx.arc(0, 0, radius, (2 * Math.PI) * (value / 100), 0, false);
-				ctx.lineWidth = stroke;
-				ctx.fillStyle = this.getColor();
-				ctx.strokeStyle = this.props.deficitFillColor;
-
-				ctx.stroke();
-			}
-
-			ctx.lineWidth = 0;
-			ctx.setTransform(1, 0, 0, 1, centerX, centerY);
-			setFont(ctx, font);
-			textbox = ctx.measureText(value);
-
-			if(this.props.showValue) {
-				font.weight = '600';
-				setFont(ctx, font);
-				ctx.fillText(value, -textbox.width / 2, font.size / 3);
-
-				font.size /= 3;
-				font.weight = '700';
-				setFont(ctx, font);
-
-				if(this.props.showPctSymbol) {
-					ctx.fillText('%', textbox.width / 2, -font.size / 4);
-				}
-			}
-		} finally {
-			ctx.restore();
-		}
-	}
-
-	drawDot = (ctx) => {
-		let node = ctx.canvas,
-			slope = node.height / node.width,
-			centerY = (node.height / 2) + (node.width / 4),
-			centerX = centerY / slope,
-			radius = Math.floor(node.width * 0.09),
-			textbox,
-			value = getGradeLetter(this.props.value),
-			nudge = this.props.pixelDensity,
-			font = {
-				size: Math.floor(radius),
-				weight: 'bold'
-			};
-
-		ctx.save();
-		try {
-
-			ctx.setTransform(1, 0, 0, 1, centerX, centerY);
-			ctx.beginPath();
-			ctx.arc(0, 0, radius, 0, 2 * Math.PI, false);
-
-			//ctx.shadowColor = '#666';
-			//ctx.shadowBlur = 5;
-			//ctx.shadowOffsetX = 1;
-			//ctx.shadowOffsetY = 1;
-
-			ctx.fillStyle = this.getColor();
-			ctx.fill();
-
-			ctx.setTransform(1, 0, 0, 1, centerX, centerY);
-			ctx.fillStyle = '#fff';
-
-			setFont(ctx, font);
-			textbox = ctx.measureText(value);
-
-			//ctx.shadowOffsetX = -1;
-			//ctx.shadowOffsetY = -1;
-
-			ctx.fillText(value, Math.floor(-textbox.width / 2) + nudge, font.size / 3);
-		} finally {
-			ctx.restore();
-		}
-	}
-}
-
-
-function setFont (context, font) {
-	context.font = [
-		font.style || 'normal',
-		font.variant || 'normal',
-		font.weight || 'normal', (font.size || 10) + 'px',
-		font.family || '"Open Sans"'
-	].join(' ');
-}
-
-function getGradeLetter (g) {
-	if (g >= 90) {
-		g = 'A';
-	} else if (g >= 80) {
-		g = 'B';
-	} else if (g >= 70) {
-		g = 'C';
-	} else if (g >= 60) {
-		g = 'D';
-	} else {
-		g = 'F';
-	}
-
-	return g;
 }
