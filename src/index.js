f1b828093223e4af4223a0448e3855c532721ccf
Ray Hatfield <ray.hatfield@nextthought.com>
2019-01-24 15:50:39 -0600

    export pie chart

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export NumericValue from './numeric-value';
 export CircularProgress from './circular-progress';
+export PieChart from './pie-chart';
 export UpdateWithFrequency from './UpdateWithFrequency';
