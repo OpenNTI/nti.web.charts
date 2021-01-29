5913ce584513411f4c913a8ae5ffc678b5c2b54e
Jonathan Grimes <jsg2021@gmail.com>
2021-01-29 09:23:36 -0600

    export DefaultAlias from => export { default as DefaultAlias } from

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export NumericValue from './numeric-value';
-export CircularProgress from './circular-progress';
-export PieChart from './pie-chart';
-export UpdateWithFrequency from './UpdateWithFrequency';
+export { default as NumericValue } from './numeric-value';
+export { default as CircularProgress } from './circular-progress';
+export { default as PieChart } from './pie-chart';
+export { default as UpdateWithFrequency } from './UpdateWithFrequency';
